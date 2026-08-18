d54fdd5fbdc9884d1eb266b95e7490d9dc3d047b
Ghulam Mujtaba <mujtabaasjad@gmail.com>
2026-08-18 22:56:30 +0500

    Switch to a data router so the note editor renders

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 import { AppShell } from '../components/layout/AppShell'
 import { Archived } from '../pages/Archived'
 import { Dashboard } from '../pages/Dashboard'
@@ -11,25 +11,23 @@ import { SignUp } from '../pages/SignUp'
 import { Trash } from '../pages/Trash'
 import { ProtectedRoute } from './ProtectedRoute'
 
-export function AppRoutes() {
-  return (
-    <Routes>
-      <Route element={<SignIn />} path="/signin" />
-      <Route element={<SignUp />} path="/signup" />
+export const appRoutes = (
+  <>
+    <Route element={<SignIn />} path="/signin" />
+    <Route element={<SignUp />} path="/signup" />
 
-      <Route element={<ProtectedRoute />}>
-        <Route element={<AppShell />}>
-          <Route element={<Dashboard />} index />
-          <Route element={<Favourites />} path="favourites" />
-          <Route element={<Archived />} path="archived" />
-          <Route element={<Trash />} path="trash" />
-          <Route element={<NoteEditorPage />} path="notes/new" />
-          <Route element={<NoteEditorPage />} path="notes/:id" />
-          <Route element={<Profile />} path="profile" />
-        </Route>
+    <Route element={<ProtectedRoute />}>
+      <Route element={<AppShell />}>
+        <Route element={<Dashboard />} index />
+        <Route element={<Favourites />} path="favourites" />
+        <Route element={<Archived />} path="archived" />
+        <Route element={<Trash />} path="trash" />
+        <Route element={<NoteEditorPage />} path="notes/new" />
+        <Route element={<NoteEditorPage />} path="notes/:id" />
+        <Route element={<Profile />} path="profile" />
       </Route>
+    </Route>
 
-      <Route element={<NotFound />} path="*" />
-    </Routes>
-  )
-}
+    <Route element={<NotFound />} path="*" />
+  </>
+)
